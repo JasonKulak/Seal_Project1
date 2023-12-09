efa7d6c74d0208c53467717c57328346769ff899
Jason Kulak <JasonKulak@yahoo.com>
2023-12-08 19:20:21 -0600

    Went rouge(one). Attempting to start from scratch in my 'two series'

---
+++ app2.js
@@ -0,0 +1,6 @@
+// const URL = "https://cdn.contentful.com/spaces/ka9x7i3b4e49/environments/master/entries?access_token=XDsz_JL64ML_sSt02_hJtwPrMhoQBy_wA21HTVxdhaA&content_type=triviaQ"
+// $.ajax(URL)
+// .then(data => {
+//     console.log(data)
+// })
+   
